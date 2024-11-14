6133f27e8416485f219b5a8ce25ca6e74e39200b
Alyssa Resh <aresh@Zipcoders-MacBook-Pro.local>
2024-11-14 14:31:50 -0500

    added methods in GameInterface

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
@@ -4,10 +4,31 @@ import com.github.zipcodewilmington.casino.GamblingInterface;
 import com.github.zipcodewilmington.casino.GameInterface;
 import com.github.zipcodewilmington.casino.PlayerInterface;
 
+import java.util.Map;
+
 /**
  * Created by leon on 7/21/2020.
  */
 public class SlotsGame implements GameInterface, GamblingInterface {
+    String[] symbols;
+    String[] reel;
+    String[] results;
+    Map<String, Integer> payoutTable;
+
+//    player decides to play
+//    player places bet
+//    player pulls handle to spin wheel
+//    wheel spins
+//    reel stops on random symbol (all three reels)
+//    see if symbols match (player wins?)
+//    compare to playtable & see what payout is
+//    player wins or loses money
+//    asks player if they'd like to play again
+
+
+
+
+
     @Override
     public void add(PlayerInterface player) {
 
